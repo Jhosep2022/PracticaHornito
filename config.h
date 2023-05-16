83f3f94738cbaee9382c4910e79df74c5858ea1d
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2023-05-16 12:20:27 -0400

    mi primer commit

---
+++ config.h
@@ -0,0 +1,7 @@
+const char* ssid = "Jowi";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password =  "josejose";    //Indicamos la contraseña de de red WiFi
+
+/*
+const char* ssid = "xxxxxx";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password = "yyyy";  //Indicamos la contraseña de de red WiFi
+*/
